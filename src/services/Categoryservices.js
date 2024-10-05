908708f12ca07b9a292afcae4a58837b03332971
conghoang204 <146950585+conghoang204@users.noreply.github.com>
2024-10-05 21:01:12 +0700

    avatar category

--- src/services/Categoryservices.js
+++ src/services/Categoryservices.js
@@ -3,8 +3,24 @@ import axios from 'axios';
 const API_URL = 'http://localhost:3000/v1/categorys';
 
 // Khởi tạo instance axios
+const getToken = () => {
+    return localStorage.getItem('token'); // Hoặc sử dụng sessionStorage nếu cần
+};
+
 const axiosInstance = axios.create({
     baseURL: API_URL,
+    headers: {
+        'Authorization': `Bearer ${getToken()}` // Thêm token vào header
+    }
+});
+
+// Cập nhật token trong headers
+axiosInstance.interceptors.request.use((config) => {
+    const token = getToken();
+    if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
 });
 
 // Lấy danh sách danh mục
